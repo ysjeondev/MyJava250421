212f27b91659b9c53f8ac141d69fe5a96dea550a
hpc99 <hpc99@DESKTOP-57PMHQP.localdomain>
2025-04-21 18:50:21 +0900

    first commit

---
+++ MyJava250421/src/MyJava250421/MyJava.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package MyJava250421;
+
+/**
+ * 
+ */
+public class MyJava {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
